907d286311e3b0286302de5e56404673933c710a
Lauren Richert <lsrichert@gmail.com>
2018-10-11 11:46:31 -0500

    finished book and library and started patron

---
+++ MyLibrary/Models/Patron.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models
+{
+    public class Patron
+    {
+        [Key]
+        public int PatronId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Display(Name="Checked Out Books")]
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+
+    }
+}
---
+++ MyLibrary/ViewModels/LibraryCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModels
+{
+    public class LibraryCreateViewModel
+    {
+        public Library Library { get; set; }
+    }
+}
